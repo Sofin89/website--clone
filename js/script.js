ee33f6fe7795911b78f364585f0b9c2463bff34f
Sofin89 <sofinmansuri056@gmail.com>
2025-02-20 18:45:12 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const menu = document.querySelector("#mobile-menu");
+const menulinks = document.querySelector(".navbar-menu");
+
+//display mobile menu
+const mobilmenu = ()=>{
+    menu.classList.toggle("is-active")
+    menulinks.classList.toggle("active")
+
+}
+menu.addEventListener("click", mobilmenu)
+
+
+
+
+
+// slide image js start  
+let slideIndex = [1];
+let slideId = ["mySlides1"]
+showSlides(1, 0);
+// show
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
+// slide image js end  
